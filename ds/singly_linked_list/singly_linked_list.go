95a388911905ecee1711f348d24676d500aa1ea0
bohdan_serdinov <bogdan.serdinov.2002@gmail.com>
2021-03-26 23:20:30 +0200

    implementand test all operations with singly linked list

--- ds/singly_linked_list/singly_linked_list.go
+++ ds/singly_linked_list/singly_linked_list.go
@@ -25,6 +25,23 @@ func (n *node) InsertInTheBeginning(data int){
 	newItem.next = head.next
 	head.next = newItem
 }
+func (n *node) InsertInTheMiddle(data , index int){
+	i := 0
+	newItem := new(node)
+	newItem.data = data
+
+	item := head.next
+	for item.next != nil {
+		i++
+		if index == i{
+			break
+		}
+		item = item.next
+	}
+
+	newItem.next = item.next
+	item.next = newItem
+}
 
 func (n *node) InsertInTheEnd(data int){
 	newItem := new(node)
@@ -39,6 +56,31 @@ func (n *node) InsertInTheEnd(data int){
 
 }
 
+func (n *node) DeleteFirst(){
+	head.next = head.next.next
+}
+
+func (n *node) DeleteLast(){
+	item := head.next
+	for item.next.next != nil {
+		item = item.next
+	}
+	item.next = nil
+}
+
+func (n *node) DeleteFromMiddle(index int){
+	i := 0
+	item := head.next
+	for item.next != nil {
+		i++
+		if index == i+1{
+			item.next = item.next.next
+		}
+		item = item.next
+	}
+
+}
+
 func (n *node) print(){
 	item := head.next
 	for item != nil {
@@ -50,7 +92,18 @@ func (n *node) print(){
 func main(){
 	newList := CreateNewList(5)
 	newList.InsertInTheBeginning(10)
+	newList.InsertInTheBeginning(12)
+	newList.InsertInTheBeginning(14)
+	newList.InsertInTheBeginning(16)
+
+	newList.InsertInTheBeginning(11)
+	newList.DeleteFirst()
+
+	newList.InsertInTheMiddle(100,3)
+
+	newList.DeleteLast()
 	newList.InsertInTheEnd(7)
 
+	newList.DeleteFromMiddle(4)
 	newList.print()
 }
