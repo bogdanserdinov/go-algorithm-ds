96b3b018505e10dff7d48a87c9e47c8b9dca378c
bohdan_serdinov <bogdan.serdinov.2002@gmail.com>
2021-03-26 21:04:30 +0200

    add implementation (basic) for stack

---
+++ ds/stack/stack.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+//const max int = 10
+
+type stack struct{
+	s []int
+	top int
+}
+
+func createNewStack() *stack{
+	s := new(stack)
+	s.top = -1
+	return s
+}
+
+func (st *stack) push(num int){
+	st.top++
+	st.s = append(st.s,num)
+}
+
+func (st *stack) isEmpty() bool{
+	if st.top == -1{
+		return true
+	} else{
+		return false
+	}
+}
+func (st *stack) pop() {
+	res := st.isEmpty()
+	if res == true {
+		fmt.Println("stack is empty")
+		return
+	}else{
+		st.s = st.s[:len(st.s)-1]
+		st.top--
+	}
+}
+func (st *stack) print(){
+	for i := 0; i < st.top+1;i++{
+		if i == st.top+1{
+			fmt.Print(st.s[i])
+			break
+		}
+		fmt.Print(st.s[i],"->")
+	}
+}
+
+func main(){
+	stack := createNewStack()
+
+	stack.push(5)
+	stack.push(10)
+	stack.pop()
+	stack.push(15)
+
+	stack.print()
+}
