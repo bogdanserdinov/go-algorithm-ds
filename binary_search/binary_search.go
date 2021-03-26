a33197fbfe596aac5010e1377fc19abf3c35d4be
bohdan_serdinov <bogdan.serdinov.2002@gmail.com>
2021-03-26 19:48:04 +0200

    add some of logic of algorithm

--- binary_search/binary_search.go
+++ binary_search/binary_search.go
@@ -1,5 +1,18 @@
 package main
 
 func main(){
+	arr := [...]int{1,2,3,4,5,6,7,8,9,10}
+	guessNum := 10
+	findNum(arr,guessNum)
+}
 
+func findNum (arr [10]int,num int) (int,int){		//returns index,number
+	min := 0
+	max := len(arr)
+
+	mid := (min+max)/2
+
+	if arr[mid] == num {
+		return mid,arr[mid]
+	}
 }
