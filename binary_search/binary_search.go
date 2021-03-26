1738e4afab3947ca77f9925fcb2ccc8d2e6396d1
bohdan_serdinov <bogdan.serdinov.2002@gmail.com>
2021-03-26 20:02:10 +0200

    add binary search algorithm

--- binary_search/binary_search.go
+++ binary_search/binary_search.go
@@ -1,18 +1,46 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"log"
+	"sort"
+)
+
 func main(){
-	arr := [...]int{1,2,3,4,5,6,7,8,9,10}
-	guessNum := 10
-	findNum(arr,guessNum)
+	arr := []int{1,2,3,4,5,6,7,8,9,10}
+	sort.Ints(arr)
+	guessNum := 1
+	index,num,err := findNum(arr,guessNum)
+	if err != nil{
+		log.Println(err.Error())
+	}
+	fmt.Println("result ->\tindex = ",index,"num = ",num)
 }
 
-func findNum (arr [10]int,num int) (int,int){		//returns index,number
+func findNum (arr []int,num int) (int,int,error){		//returns index,number
 	min := 0
-	max := len(arr)
+	max := len(arr)-1
 
-	mid := (min+max)/2
+	//mid := (min+max)/2
+	var mid int
 
-	if arr[mid] == num {
-		return mid,arr[mid]
+	for min <= max{
+		mid = (min+max)/2
+		if arr[mid] == num {
+			return mid,arr[mid],nil
+
+		}
+		if	arr[mid] > num {
+			min = mid + 1
+			//mid = (min+max)/2
+		}
+		if	arr[mid] < num {
+			max = mid - 1
+			//mid = (min+max)/2
+		}
 	}
+	err := errors.New("could not find guess number")
+	return 0,0,err
+
 }
