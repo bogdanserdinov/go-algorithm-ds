15daf03fe8ce3f705ccedb58c92efe8e3022a328
bohdan_serdinov <bogdan.serdinov.2002@gmail.com>
2021-03-27 19:32:11 +0200

    add implementation of enqueue and dequeue, test that methods

--- ds/queue/queue.go
+++ ds/queue/queue.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const max int = 5	//max size of queue
 
 type queue struct {
@@ -32,6 +34,50 @@ func(q *queue) isEmpty() bool{
 	}
 }
 
+func(q *queue) enqueue(data int) {
+	if q.isFull(){
+		fmt.Println("queue is full, could not insert new data")
+		return
+	}else {
+		if q.front == -1 {
+			q.front++
+		}
+		q.items = append(q.items, data)
+		q.rear++
+	}
+}
+
+func(q *queue) dequeue(){
+	if q.isEmpty(){
+		fmt.Println("queue is empty, could not delete data")
+		return
+	}else{
+		if q.front >= q.rear{	//if in queue was only 1 element
+			q.front = -1
+			q.rear = -1
+		}else{
+			q.front++
+		}
+	}
+}
+
+func(q *queue) print(){
+	for i:= q.front;i <= q.rear;i++{
+		if i == q.rear{
+			fmt.Print(q.items[i])
+			break
+		}
+		fmt.Print(q.items[i],"<-")
+	}
+}
+
 func main() {
-	new := newQueue()
+	q := newQueue()
+	q.enqueue(10)
+	q.enqueue(11)
+	q.enqueue(12)
+	q.enqueue(13)
+
+	q.dequeue()
+	q.print()
 }
