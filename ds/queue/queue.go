f80a46622d9e7aee2bf22a602737a0c1300f5242
bohdan_serdinov <bogdan.serdinov.2002@gmail.com>
2021-03-27 19:11:14 +0200

    add basic methods and constructor for queue

--- ds/queue/queue.go
+++ ds/queue/queue.go
@@ -1,5 +1,37 @@
 package main
 
-func main(){
+const max int = 5	//max size of queue
 
+type queue struct {
+	front int
+	rear int
+	items []int
+}
+
+func newQueue() *queue{
+	res := new(queue)
+	res.rear = -1
+	res.front = -1
+
+	return res
+}
+
+func(q *queue) isFull() bool{
+	if q.rear == max - 1{
+		return true
+	}else {
+		return false
+	}
+}
+
+func(q *queue) isEmpty() bool{
+	if	q.front == -1 && q.rear == -1{
+		return true
+	}else{
+		return false
+	}
+}
+
+func main() {
+	new := newQueue()
 }
